bf96faae50584eb168466904ff85dacfbc14f26b
Alexandre Martin <alexandre@alkivi.fr>
2020-04-30 15:46:04 +0200

    added timer per agent feature

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function alertFreshdesk(message) {
+	console.error(message);
   return client.interface.trigger("showNotify", {
     type: "alert", title: "Try again",
     message
@@ -12,7 +13,7 @@ $(document).ready( function() {
   .then(function(_client) {
     var client = _client;
     window.client = client;
-
+    
     // First we need ticketData
     getTicketData()
     .then(function(ticketData) {
@@ -27,18 +28,17 @@ $(document).ready( function() {
       // 1 - get timer_step
       // 2 - createFirstTimeEntry before starting automatic update
       const timerStep = getTimerStep();
-      const firstTimeEntry = createFirstTimeEntryIfNeeded(ticketData);
+      const timeEntryID = checkTimeEntries(ticketData);
 
       // Once initial check is OK, start automatic update
-      Promise.all([timerStep, firstTimeEntry])
+      Promise.all([timerStep, timeEntryID])
       .then(function(values) {
         const step = values[0];
-        const firstTimeEntryTest = values[1];
-        if (!firstTimeEntryTest) {
-          return alertFreshdesk('No timeEntry, this is weird');
+        const timeEntryID = values[1];
+        if (!timeEntryID) {
+          return alertFreshdesk('Error time entry not returned');
         }
-        return timerUpdateLoop(step);
-
+        return timerUpdateLoop(step, timeEntryID);
       })
       .catch(function() {
         return alertFreshdesk('Error getting promiseAll');
@@ -68,7 +68,6 @@ const getTimerStep = function getTimerStep() {
   return client.iparams.get("timer_step")
   .then(function(data) {
     var timerStep = parseInt(data.timer_step);
-    console.log("Timer step is " + timerStep + " minute(s)");
     timerStep = timerStep * 60 * 1000;
     console.debug("Timer step is " + timerStep + " ms");
     return timerStep
@@ -78,32 +77,42 @@ const getTimerStep = function getTimerStep() {
   });
 }
 
-const createFirstTimeEntryIfNeeded = function createFirstTimeEntryIfNeeded(ticketData) {
-  console.debug("createFirstTimeEntryIfNeeded");
+const checkTimeEntries = function checkTimeEntries(ticketData) {
+  console.debug("checkTimeEntries");
   return client.data.get("time_entry")
-  .then(function(data){
-    const numberOfTimeEntries = data.time_entry.time_entries.length;
-    console.debug('We have ' + numberOfTimeEntries + ' time_entries');
-    if (numberOfTimeEntries === 0) {
-      return createFirstTimeEntry(ticketData);
-    } else {
-      return true;
-    }
-
+  .then(function(entriesData) {
+    return client.data.get("loggedInUser").then(
+      function(userData) {
+        const numberOfTimeEntries = entriesData.time_entry.time_entries.length;
+        const entries = entriesData.time_entry
+        console.debug('We have ' + numberOfTimeEntries + ' time_entries');
+        if (numberOfTimeEntries === 0)
+          return createTimeEntry(ticketData, userData.loggedInUser.id);
+        else {
+	  var time_entry = entriesData.time_entry.time_entries.find(time_entry => time_entry.agent_id === userData.loggedInUser.id);
+          if(time_entry == undefined)
+            return createTimeEntry(ticketData, userData.loggedInUser.id);
+          else
+            return time_entry.id;
+        }
+      })
+      .catch(function(){
+        return alertFreshdesk('Error getting user_data');
+      });
   })
   .catch(function(){
     return alertFreshdesk('Error getting time_entry')
   });
 };
 
-var createFirstTimeEntry = function createFirstTimeEntry(ticketData) {
-  console.debug('createFirstTimeEntry');
+var createTimeEntry = function createTimeEntry(ticketData, userId) {
+  console.debug('createTimeEntry');
   var baseUrl = "<%= iparam.freshdesk_domain %>/api/v2/tickets/";
   var url = baseUrl.concat(ticketData.ticket.id, "/time_entries");
   var body = {
     billable: false,
     timer_running: false,
-    agent_id: ticketData.responder_id,
+    agent_id: userId,
     time_spent: "00:01"
   }
 
@@ -115,9 +124,9 @@ var createFirstTimeEntry = function createFirstTimeEntry(ticketData) {
     body: JSON.stringify(body)
   };
   return client.request.post(url, options)
-  .then(function() {
-    console.debug('First time entry created');
-    return true;
+  .then(function(data) {
+    console.debug('time entry ', JSON.parse(data.response).id, ' created');
+    return JSON.parse(data.response).id;
   })
   .catch(function(e) {
     console.error('Error while posting first time entry', e);
@@ -125,11 +134,11 @@ var createFirstTimeEntry = function createFirstTimeEntry(ticketData) {
   })
 };
 
-const timerUpdateLoop = function timerUpdateLoop(step) {
+const timerUpdateLoop = function timerUpdateLoop(step, timeEntryID) {
   console.debug('timerUpdateLoop');
   Promise.resolve()
   .then(function resolver() {
-    return timerUpdatePromiseWithTimeout(step)
+    return timerUpdatePromiseWithTimeout(step, timeEntryID)
     .then(resolver)
     .catch(function(){
       return alertFreshdesk('Error timerUpdateLoop resolver');
@@ -140,28 +149,30 @@ const timerUpdateLoop = function timerUpdateLoop(step) {
   });
 };
 
-var timerUpdatePromiseWithTimeout = function timerUpdatePromiseWithTimeout(step) {
+var timerUpdatePromiseWithTimeout = function timerUpdatePromiseWithTimeout(step, timeEntryID) {
   console.debug('timerUpdatePromiseWithTimeout');
   const timerPromise = new Promise(function(resolve) {
     setTimeout(function() {
-       resolve(timerUpdate(step));
+       resolve(timerUpdate(step, timeEntryID));
     }, step)
   });
   return timerPromise
 }
 
-var timerUpdate = function timerUpdate(step) {
+var timerUpdate = function timerUpdate(step, timerEntryID) {
   return client.data.get("time_entry")
-  .then(function(data){
-    var firstTimeEntry = data.time_entry.time_entries[0];
-    console.debug("Going to add time to entry", firstTimeEntry)
-    var totalMinutes = Math.floor(firstTimeEntry.time_spent/60) + step/60000;
+  .then(function(data) {
+    var time_entry = data.time_entry.time_entries.find(time_entry => time_entry.id === timerEntryID);
+    if (time_entry == undefined)
+      return alertFreshdesk('Error cannot retreive timer');
+    console.debug("Going to add time to entry", time_entry)
+    var totalMinutes = Math.floor(time_entry.time_spent/60) + step/60000;
     var totalHours = Math.floor(totalMinutes/60);
     totalMinutes = totalMinutes - (totalHours*60);
     const timeSpent = String(totalHours).padStart(2, '0') + ":" + String(totalMinutes).padStart(2, '0');
     console.debug("time spent ", timeSpent);
     var baseUrl = "<%= iparam.freshdesk_domain %>/api/v2/time_entries/";
-    var url = baseUrl.concat(firstTimeEntry.id);
+    var url = baseUrl.concat(time_entry.id);
     var body = {
       time_spent: timeSpent
     }
